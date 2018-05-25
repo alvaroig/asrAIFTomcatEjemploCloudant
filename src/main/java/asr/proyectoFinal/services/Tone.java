e40e11aed831e8cb2fc8389125500cacb373a6fe
Alvaro Iglesias Fernandez <aif45@hotmail.com>
2018-05-25 13:15:18 +0200

    ANALISIS DE SENTIMIENTO HEHCO

--- src/main/java/asr/proyectoFinal/services/Tone.java
+++ src/main/java/asr/proyectoFinal/services/Tone.java
@@ -3,10 +3,6 @@ package asr.proyectoFinal.services;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject; 
-import com.google.gson.JsonParser; 
 
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
@@ -18,7 +14,6 @@ public class Tone {
 	public static String tono(String text) { 
 		ToneAnalyzer service = new ToneAnalyzer("2018-05-25");
 		service.setUsernameAndPassword("9c31467e-fb00-46a9-bf5b-a51d0b0615f6", "2stp5I6gsZ14");
-		
 		ToneOptions toneOptions = new ToneOptions.Builder().text(text).build();
 		ToneAnalysis tone = service.tone(toneOptions).execute();
 		String tono = tone.getDocumentTone().getTones().get(0).getToneName().toString();	
